5aff78d7654c2a6430ff6f6f024596cb9344994f
forniaiacarmen <forniaiacarmen@gmail.com>
2025-07-11 08:08:24 +0200

    quitado credenciales en texto plano

--- src/main/java/com/example/HibernateSpringCache/HibernateSpringCacheApplication.java
+++ src/main/java/com/example/HibernateSpringCache/HibernateSpringCacheApplication.java
@@ -1,5 +1,6 @@
 package com.example.HibernateSpringCache;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,10 @@ import org.springframework.cache.annotation.EnableCaching;
 public class HibernateSpringCacheApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(HibernateSpringCacheApplication.class, args);
 	}
 
